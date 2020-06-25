13f3aa3a4cd9fd436573ad189eb8fabd83abbc77
Rakib Hossain Saif <61383258+RakibHossainSaif@users.noreply.github.com>
2020-06-25 21:52:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function myContent(){
+ 	document.getElementById("text").innerHTML = "apnara ghum theke uthun !!";
+}
+function myTime(){
+ 	document.getElementById("time").innerHTML = Date();
+}
+function myContentColor(){
+ 	document.getElementById("color").style.color="red";
+}
+function myContentFs(){
+ 	document.getElementById("fs").style.fontSize="55px";
+}
+function bulbOn(){
+ 	document.getElementById("bulb").src="images/on.gif"; 
+}
+function bulbOff(){
+ 	document.getElementById("bulb").src="images/off.gif"; 
+}
+function myContentHidden(){
+	document.getElementById("hidden").style.display="none";
+}
+function myContentShow(){
+	document.getElementById("show").style.display="block";
+}
